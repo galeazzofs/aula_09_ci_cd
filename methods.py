e54c7c77235a71e8928d318604a7ffc4e2943778
DIOGO PAVAN REMORINO <22.01750-0@maua.br>
2023-05-03 09:56:59 -0300

    exercicio calculadora

--- methods.py
+++ methods.py
@@ -5,3 +5,24 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+
+def subtrai(a,b):
+    sub = a - b
+    return sub
+
+def multiplicacao(a, b):
+    mult = a * b
+    return mult
+
+def elevaQuadrado(a):
+    quad = a*a
+    return quad
+
+def divisao(a,b):
+    div = a/b
+    return div
+
+def soma(a, b):
+    soma = a + b
+    return soma
--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,43 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_soma():
+    a = 5
+    b = 8
+
+    output = methods.soma(a,b)
+
+    assert output == 13
+
+
+def test_multiplicacao():
+    a = 7
+    b = 9
+
+    output = methods.multiplicacao(a,b)
+
+    assert output == 63
+
+def test_elevaQuadrado():
+    a = 8
+
+    output = methods.elevaQuadrado(a)
+
+    assert output == 64
+
+def test_divisao():
+    a = 12
+    b = 2
+
+    output = methods.divisao(a,b)
+
+    assert output == 6
+
+def test_subtrai():
+    a = 9
+    b = 3
+
+    output = methods.subtrai(a,b)
+
+    assert output == 6
